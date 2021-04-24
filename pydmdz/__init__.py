950bfead4ee80ebbefce38bda110d57b42a34717
Shervin Sahba <shervin@tuta.io>
2021-04-23 18:23:50 -0700

    updated email to UW (prior commit was a mistake)

--- pydmdz/__init__.py
+++ pydmdz/__init__.py
@@ -6,7 +6,7 @@ __author__ = "Shervin Sahba"
 __copyright__ = "Copyright 2020-"
 __license__ = "MIT"
 __version__ = "0.1"
-__mail__ = "shervin@tuta.io"
+__mail__ = "ssahba@uw.edu"
 __status__ = "Testing"
 
 from .dmd import *
